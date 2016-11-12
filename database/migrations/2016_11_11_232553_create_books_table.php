f0686bffdd9eed448ad2b4ce9c656ca2bfb8def4
Jose Mari Ramos <ramosjosemari@gmail.com>
2016-11-12 09:40:33 +0800

    Add basic tables

---
+++ database/migrations/2016_11_11_232553_create_books_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function ($table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('year');
+            $table->string('isbn');
+            $table->string('author');
+            $table->string('category');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
