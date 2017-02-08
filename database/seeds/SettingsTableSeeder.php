78360aa68e1c9f93347dd89971d4980ab7bbfc81
Joshua B. Turingan <turingan.joshua@gmail.com>
2017-02-08 17:01:18 +0800

    add settings table

---
+++ database/seeds/SettingsTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Setting;
+
+class SettingsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Setting::truncate();
+
+        Setting::create([
+            'title' => 'Student Books',
+            'value' => 3
+        ]);
+
+        Setting::create([
+            'title' => 'Employee Books',
+            'value' => 20
+        ]);
+
+        Setting::create([
+            'title' => 'Student Duration',
+            'value' => 3
+        ]);
+
+        Setting::create([
+            'title' => 'Employee Duration',
+            'value' => 7
+        ]);
+    }
+}
