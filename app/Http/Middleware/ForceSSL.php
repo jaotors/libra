890c2e41f4bc30abeb88e94f9fb8c8aabbea7c08
Jose Mari Ramos <ramosjosemari@gmail.com>
2017-02-12 00:30:22 +0800

    Fix #2 on ssl

--- app/Http/Middleware/ForceSSL.php
+++ app/Http/Middleware/ForceSSL.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use App;
 
 class ForceSSL
 {
@@ -16,7 +17,7 @@ class ForceSSL
     public function handle($request, Closure $next)
     {
 
-        $header = $request::header('x-forwarded-proto') <> 'https';
+        $header = $request->header('x-forwarded-proto') <> 'https';
         if ($header && !App::environment('local', 'staging')) {
             return redirect()->secure($request->getRequestUri());
         }
